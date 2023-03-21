93ffd9926ecfa9bcb711047d25c1f9ab8d230862
Iaroslav Leiman <yaroslavleiman@yandex.ru>
2023-03-21 21:32:07 +0700

    Added source code

--- TS_Task_1/index.ts
+++ TS_Task_1/index.ts
@@ -1,3 +1,24 @@
+// Исходный код:
+// const currRate = "1.05";
+
+// const fetchCurr = (response) => {
+//   const data = JSON.parse(response);
+//   return data;
+// };
+
+// function transferEurToUsd(available, amount, commission) {
+//   if (available) {
+//     let res = fetchCurr(currRate) * amount * commission;
+//     console.log(res);
+//   } else {
+//     console.log("Сейчас обмен недоступен");
+//   }
+// }
+
+// transferEurToUsd(true, 500, 1.05);
+
+
+// Решение с типизацией:
 const currRate: string = "1.05";
 
 const fetchCurr = (response: string): number => {
